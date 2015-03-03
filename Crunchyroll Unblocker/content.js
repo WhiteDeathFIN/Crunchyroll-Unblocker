896ec76a6adebdafb74dc5084c0c27eb2acfb477
jerryteps <jerryteps@users.noreply.github.com>
2015-03-03 12:21:38 +1100

    Update content.js
    Changed to update based on the country flag DOM element.

--- Crunchyroll Unblocker/content.js
+++ Crunchyroll Unblocker/content.js
@@ -1,20 +1,20 @@
 var cr = new RegExp("crunchyroll.");
-var uscr = new RegExp("crunchyroll.com");
+var us = new RegExp("United States of America");
+var c = window.location.hostname;
 
-if (cr.test(document.URL) && document.URL.indexOf("crunchyroll.") < 13) {
-	if (uscr.test(document.URL)) {
-		_setCookie ();
-	} else {
-		chrome.runtime.sendMessage({msg: "REDIRECT"});
-	}
-}
-		
-function _setCookie () {
+if (cr.test(window.location.hostname)) {
 	chrome.storage.local.get("TIME", function (items) {
 		var time = items["TIME"];
-		if ((time + 1800000) < (new Date()).getTime()) {
+		if ((time + 1800000) < (new Date()).getTime() || !(isUS())) {
 			chrome.storage.local.set({"TIME": (new Date()).getTime()});
-			chrome.runtime.sendMessage({msg: "SET"});
+			chrome.runtime.sendMessage({msg: c.slice(c.indexOf("crunchyroll.") + 11, c.length)});
 		}
 	});
+}
+
+function isUS () {
+	var location = document.getElementById("footer_country_flag");
+	if (us.test(location.alt)) {
+		return true;
+	}
 }
