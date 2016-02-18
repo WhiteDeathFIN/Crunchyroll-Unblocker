ad903979ab2493a9e0af2888715d386e83ee3394
Santeri Hetekivi <santeri@hotmail.com>
2016-02-18 17:28:04 +0200

    Changed the page that loads on click to queue page.

--- Crunchyroll Unblocker/background.js
+++ Crunchyroll Unblocker/background.js
@@ -3,20 +3,22 @@ function setCookie (tld) {
 	xhr.timeout = 5000;
 	xhr.ontimeout = function () {
 		chrome.notifications.create("", {type: "basic", title: "Request Timed Out", message: "Crunchyroll Unblocker has encounted an error", iconUrl: "crunblock128.png"}, function (notificationId) {});
-	}
+	};
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState == 4) {
 			chrome.cookies.remove({url: "http://crunchyroll" + tld + "/", name: "sess_id"});
 			chrome.cookies.set({url: "http://.crunchyroll" + tld + "/", name: "sess_id", value: xhr.responseText});
 		}
-	}
+	};
 	xhr.open("GET", "http://www.crunblocker.com/sess_id.php", true);
 	xhr.send(null);
 }
 
 chrome.browserAction.onClicked.addListener (function () {
 	setCookie(".com");
-	chrome.tabs.create({url: "http://crunchyroll.com/videos/anime/"});
+	// Page that opens when done.
+	// This fork sets this to queue page.
+	chrome.tabs.create({url: "http://www.crunchyroll.com/home/queue"});
 });
 
 chrome.runtime.onMessage.addListener(function (message) { 
--- README.md
+++ README.md
@@ -2,3 +2,10 @@ Crunchyroll-Unblocker
 =====================
 
 Crunchyroll Unblocker extension for Chrome.
+
+
+
+Fork by: Santeri Hetekivi
+=====================
+
+Page that loads on clock set to queue page.
